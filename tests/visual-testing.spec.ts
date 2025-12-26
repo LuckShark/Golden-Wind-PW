a06901e827de09d0bd0b3a373fec4b101c9c66a0
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2025-12-26 18:34:06 -0300

    visual check

---
+++ tests/visual-testing.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from "@playwright/test";
+
+//para atualizar um snapshot alterado: npx playwright test --update-snapshots
+
+test.describe("Visual Testing", () => {
+  test("Plain screenshot captured of Login page", async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/login");
+    await expect(page).toHaveScreenshot();
+    console.log("Baseline screenshot captured sucessfully");
+  });
+
+  test("Full page screenshot captured of Login page", async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/login");
+    await expect(page).toHaveScreenshot({ fullPage: true });
+  });
+
+  //Visual check specific element
+  test("Visual check of login button", async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/login");
+    const loginButton = page.getByRole("button", { name: "Login" });
+    await expect(loginButton).toHaveScreenshot("login-button-screenshot.png");
+  });
+
+  //Masking sensitive information in screenshots
+  test.only("Masking screenshot of login page", async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/login");
+    await expect(page).toHaveScreenshot("login-info-masked.png", {
+      fullPage: true,
+      mask: [page.locator("#username"), page.locator("#password")],
+    });
+  });
+});
